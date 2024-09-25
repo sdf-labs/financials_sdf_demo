5c9b5b47beb954352edc3714820067ba22236809
Elias DeFaria <eliasdefaria.45@gmail.com>
2024-09-24 19:42:32 -0700

    Moved back to no quotes for repro

--- models/financials/public_operations/fct_orders.sql
+++ models/financials/public_operations/fct_orders.sql
@@ -7,7 +7,7 @@ select
   ord.OrderStatus,
   ord.OrderPlacedTimestamp,
   7 as Ordr
-from "financials"."public_staging"."stg_furniture_mart_orders" as ord
+from financials.public_staging.stg_furniture_mart_orders as ord
 -- uncomment the join criteria below to implement a time range join. 
 --   and ord.OrderPlacedTimestamp between cus.dbt_valid_from and ifnull(cus.dbt_valid_to, '2099-12-31'::timestamp_ntz)
 
--- models/financials/public_staging/int_orders_items_products_joined.sql
+++ models/financials/public_staging/int_orders_items_products_joined.sql
@@ -3,4 +3,4 @@
 select
   itm.OrderId,
   1000 as Price
-from "financials"."public_staging"."stg_furniture_mart_orderitems" as itm
+from financials.public_staging.stg_furniture_mart_orderitems as itm
