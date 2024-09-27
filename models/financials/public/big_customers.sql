1ab054c6789895f7fe2f018bdba6557894bcf466
Elias DeFaria <eliasdefaria.45@gmail.com>
2024-09-26 19:54:00 -0700

    Updated big and small customers

--- models/financials/public/big_customers.sql
+++ models/financials/public/big_customers.sql
@@ -1,5 +1,5 @@
 {% set small_customers = "('McDonalds', 'Walmart', 'Amazon')" %} 
-WITH stg_crm_customers AS (
+WITH crm_cus AS (
 {#-
     Normally we would select from the table here, but we are using seeds to load
     our data in this project
@@ -12,6 +12,6 @@ from "public_staging"."stg_crm_customers"
 
 select
   *
-from stg_crm_customers as cus
+from crm_cus as cus
 where
   cus.FullName not in {{ small_customers }}
